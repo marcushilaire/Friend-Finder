228f69bda86fbeeb1acafa83c349614f5b426371
unknown <marcushilaire@gmail.com>
2018-03-12 18:59:09 -0400

    full post functionality

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,11 @@
-var express= require("express");
-var fs = require('fs')
+// var express= require("express");
+// var fs = require('fs')
 var path = require('path')
-var bodyParser= require("body-parser")
-var app = express()
+// var bodyParser= require("body-parser")
+// var app = express()
 var friends = require("../data/friends.js")
-console.log("api Route")
-console.log(friends.friends[0])
+// console.log("api Route")
+// console.log(friends.friends)
 
 module.exports = function(app){
     app.get("/api/friends", function(req, res){
@@ -13,7 +13,8 @@ module.exports = function(app){
     })
     app.post("/api/friends", function(req,res){
         var newFriend = req.body
-        friends.push(newFriend)
+        friends.friends.push(newFriend)
+        console.log(newFriend)
     })
 }
 // console.log(__dirname)
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,13 +1,7 @@
-//var express= require("express");
+
 var path = require("path");
-// var fs = require('fs')
-// var bodyParser= require("body-parser")
-// var app = express();
 console.log("html route")
 
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(bodyParser.json());
-// app.use(express.static(path.join(__dirname, 'public')));
 module.exports = function(app){
   
     app.get("/survey", function(req, res){
@@ -18,11 +12,3 @@ module.exports = function(app){
         res.sendFile(path.join(__dirname + "/../public/home.html"));
     });
 }
-
-// app.get("/"), function(req, res){
-//     res.sendFile(path.join(__dirname, "./home.html"))
-// }
-
-// app.listen(PORT, function(){
-//     console.log("listeningon http://localhost:" + PORT);
-// })ç
